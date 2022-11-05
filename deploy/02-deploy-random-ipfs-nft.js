460b0e395b1e0aca7a72a40c40895e32c441756e
Aboudoc <aboutika.reda@gmail.com>
2022-11-05 09:14:31 +0100

    init commit

--- deploy/02-deploy-random-ipfs-nft.js
+++ deploy/02-deploy-random-ipfs-nft.js
@@ -17,11 +17,18 @@ const metadataTemplate = {
     ],
 }
 
+let tokenUris = [
+    "ipfs://QmaVkBn2tKmjbhphU7eyztbvSQU5EXDdqRyXZtRhSGgJGo",
+    "ipfs://QmYQC5aGZu2PTH8XzbJrbDnvhj3gVs7ya33H9mqUNvST3d",
+    "ipfs://QmZYmH5iDbD6v3U2ixoVAjioSzvWJszDzYdbeCLquGSpVm",
+]
+
+const FUND_AMOUNT = "10000000000000000000" // 10 LINK
+
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
-    let tokenUris
 
     // get the IPFS hashes of our images
     if (process.env.UPLOAD_TO_PINATA == "true") {
@@ -36,6 +43,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         const tx = await vrfCoordinatorV2Mock.createSubscription()
         const txReceipt = await tx.wait(1)
         subscriptionId = txReceipt.events[0].args.subId
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
     } else {
         vrfCoordinatorAddress = networkConfig[chainId].vrfCoordinatorV2
         subscriptionId = networkConfig[chainId].subscriptionId
