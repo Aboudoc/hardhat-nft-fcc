2004f1a53a96ee7dcd002e6c3c9ca41e19968fc2
Aboudoc <aboutika.reda@gmail.com>
2022-11-11 09:26:09 +0100

    svg

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,6 +1,9 @@
 const { network } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
 
+const DECIMALS = "18"
+const INITIAL_PRICE = ethers.utils.parseUnits("2000", "ether")
+
 const BASE_FEE = ethers.utils.parseEther("0.25")
 const GAS_PRICE_LINK = 1e9
 
@@ -11,10 +14,17 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
 
     if (developmentChains.includes(network.name)) {
         log("Local network detected! Deploying mocks...")
+
         await deploy("VRFCoordinatorV2Mock", {
             from: deployer,
             log: true,
             args: args,
+        })
+
+        await deploy("MockV3Aggregator", {
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_PRICE],
         })
 
         log("Mocks Deployed!")
@@ -22,4 +32,4 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     }
 }
 
-module.exports.tags = ["all", "mocks"]
+module.exports.tags = ["all", "mocks", "main"]
